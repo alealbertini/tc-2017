ee4a434057d3452932164f8fe35891842a3a77e1
root <root@crunch>
2017-05-30 00:37:31 -0300

    test

--- tp2/src/parte1.py
+++ tp2/src/parte1.py
@@ -13,23 +13,29 @@ def main():
 	global args
 	parser = argparse.ArgumentParser(description='tdc tp2 parte 1')
 	parser.add_argument('--dest', default='mit.edu', help = 'destino')
-	parser.add_argument('--timeout', default='1', help = 'timeout')
+	parser.add_argument('--timeout', default=1.0, help = 'timeout')
+	parser.add_argument('--maxhops', default=50, help = 'maximo numero de saltos')
 	args = vars(parser.parse_args())
     
 	ttl = 1
-	max_hops = 50
-	while max_hops > 0:
+	hops = int(args.get('maxhops'))
+	while hops > 0:
+		print "TTL=", "%3d" % ttl,
+		sys.stdout.flush()
 		pid =  100+ttl
-		ans = sr1(IP(dst=args.get('dest'), ttl=ttl) / ICMP(id = pid), verbose=False, timeout=float(args.get('timeout')))
-		if ans is None:
-			print "???"
-		else:	
-			print toString(ans)
-			if ans[ICMP].type == 0:
-				break
+		ans, unans = sr(IP(dst=args.get('dest'), ttl=ttl) / ICMP(id = pid), verbose=False, timeout=float(args.get('timeout')))
+		
+		if len(ans) > 0:
+			(req, res) = ans[0]
+			print "->", "\t", toString(res), (res.time-req.sent_time) 
+			if res[ICMP].type == 0:
+				break # llegamos
+		else:
+			print "->", "\t", "Timeout"
+		sys.stdout.flush()
 		ttl = ttl + 1
-		max_hops = max_hops - 1
-			
+		hops = hops - 1
+
 
 if __name__ == '__main__':
     main()
